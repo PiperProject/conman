f77ac3d12fc3b5d55e1978255a4bdbe2c7add340
Kathryn Dahlgren <kmdahlgr@ucsc.edu>
2018-08-11 09:42:43 -0700

    edits

--- src/ConMan.py
+++ src/ConMan.py
@@ -27,28 +27,48 @@ import settings
 
 
 class ConMan( object ) :
+  """ConMan : A General constraint management tool for NoSQL systems.
 
+  Issue queries to an underlying database using the Query class.
+  When invoked, ConMan captures queries issued to the Query class
+  to analyze inputs and deletion results for data integrity violations.
+  Queries violating data integrity return an error.
+  Users educate ConMan on data integrity definitions 
+  for three different types of constraints:
+    Primary Key
+    Foreign Key
+    Data Type
+  Given NoSQL systems often manifest in the absense of a well-defined
+  schema, key constraints operate over the looser notion of identifying 
+  attributes of stored data.
+  """
 
   ################
   #  ATTRIBUTES  #
   ################
   nosql_type    = None   # the type of nosql database under consideration
-  ontology      = None   # an rdflib Graph object instance
-  MONGOSAVEPATH = None
-
 
   ##########
   #  INIT  #
   ##########
   def __init__( self, nosql_type ) :
+    """Constructor"""
 
     # save nosql db type
     self.nosql_type = nosql_type
 
-    # instantiate the ontology graph
-    self.ontology  = rdflib.Graph()
-
     logging.debug( "  ...instantiated ConMan instance." )
+
+
+  #  
+
+
+#########################
+#  THREAD OF EXECUTION  #
+#########################
+if __name__ == "__main__" :
+  c = ConMan( "pickledb" )
+
 
 #########
 #  EOF  #
