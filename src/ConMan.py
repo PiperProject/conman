a11928008ddb4eea48c6c83e1e177b207f20b4ab
Kathryn Dahlgren <kmdahlgr@ucsc.edu>
2018-08-18 19:41:17 -0700

    drafted pickledb implementation for pk,fk,dm cons.

--- src/ConMan.py
+++ src/ConMan.py
@@ -7,7 +7,12 @@
 ##########################################################################
 
 # -------------------------------------- #
-import logging, os, pprint, pydot, rdflib, string, sys, time
+import ast
+import logging
+import os
+import string
+import sys 
+import time 
 
 # import sibling packages HERE!!!
 
@@ -37,8 +42,8 @@ class ConMan( object ) :
   for three different types of constraints:
     Primary Key
     Foreign Key
-    Data Type
-  Given NoSQL systems often manifest in the absense of a well-defined
+    Domain
+  Given NoSQL systems often manifest in the absence of a well-defined
   schema, key constraints operate over the looser notion of identifying 
   attributes of stored data.
   """
@@ -46,18 +51,291 @@ class ConMan( object ) :
   ################
   #  ATTRIBUTES  #
   ################
-  nosql_type    = None   # the type of nosql database under consideration
+  nosql_type           = None # the type of nosql database under consideration
+  cursor               = None
+  OPTIMIZATION_PATTERN = None
+  pk_cons = []
+  fk_cons = []
+  dm_cons = {}
 
   ##########
   #  INIT  #
   ##########
-  def __init__( self, nosql_type ) :
+  def __init__( self, nosql_type, cursor, OPTIMIZATION_PATTERN ) :
     """Constructor"""
 
-    # save nosql db type
     self.nosql_type = nosql_type
+    self.cursor     = cursor
+    self.OPTIMIZATION_PATTERN = OPTIMIZATION_PATTERN
 
     logging.debug( "  ...instantiated ConMan instance." )
+
+  ############
+  #  INSERT  #
+  ############
+  def insert( self, key, val ) :
+    '''input the insert query and check constraints before pushing
+    to the database'''
+
+    if type( val ) == list :
+      self.cursor.set( key, val )
+
+    elif self.check_pk_cons( key, val ) and \
+         self.check_fk_cons( key, val ) and \
+         self.check_dm_cons( key, val ) :
+
+      print "pk = " + str( self.check_pk_cons( key, val ) )
+      print "fk = " + str( self.check_fk_cons( key, val ) )
+      print "dm = " + str( self.check_dm_cons( key, val ) )
+
+      logging.debug( "  INSERT : inserting " + str(key) + ", " + str(val) )
+      self.cursor.set( key, val ) # use adapters to generalize
+
+  ##############
+  #  SET CONS  #
+  ##############
+  def set_cons( self, input_constraint ) :
+    '''input a constranint spec'''
+
+    if "pk" in input_constraint :
+      self.set_pk_cons( input_constraint )
+    elif "fk" in input_constraint :
+      self.set_fk_cons( input_constraint )
+    elif "dm" in input_constraint :
+      self.set_dm_cons( input_constraint )
+    else :
+      raise ValueError( "wtf???" )
+
+  #################
+  #  SET PK CONS  #
+  #################
+  def set_pk_cons( self, input_constraint ) :
+    self.pk_cons.append( self.parse_con( input_constraint ) )
+
+  #################
+  #  SET FK CONS  #
+  #################
+  def set_fk_cons( self, input_constraint ) :
+    self.fk_cons.append( self.parse_con( input_constraint ) )
+
+  #################
+  #  SET DM CONS  #
+  #################
+  def set_dm_cons( self, input_constraint ) :
+    parsed_con = self.parse_con( input_constraint )
+
+    att = parsed_con[1]
+    typ = parsed_con[2]
+
+    # TODO : error checking
+    self.dm_cons[ att ] = typ 
+
+
+  ###################
+  #  CHECK PK CONS  #
+  ###################
+  def check_pk_cons( self, key, val ) :
+    '''check for primary key constraint violations'''
+
+    logging.debug( "  CHECK PK CONS : key = " + str( key ) )
+    logging.debug( "  CHECK PK CONS : val = " + str( val ) + ", type = " + str( type( val ) ) )
+    logging.debug( "  CHECK PK CONS : pk_cons :")
+    for k in self.pk_cons :
+      logging.debug( str( k ) )
+
+    # need to make sure pk cons in key/val hold relative to 
+    # existing db data
+    # => need to query database in base_case or query
+    #    support structures in optimized cases.
+
+    if self.OPTIMIZATION_PATTERN.lower() == "base_case" :
+
+      if len( self.cursor.getall() ) < 1 :
+        print "here2"
+        return True
+
+      elif type( val ) == dict :
+
+        for k1 in val :
+          if self.is_pk( k1 ) :
+            logging.debug( "  CHECK PK CONS : k1 = " + str( k1 ) )
+            # TODO : generalize backend
+            for k2 in self.cursor.getall() :
+              k2_val = self.cursor.get( k2 )
+              if type( k2_val ) == dict :
+                logging.debug( "  CHECK PK CONS : k2 = " + str( k2_val ) )
+                existing_val = self.convert_to_list( str( k2_val ) )
+                logging.debug( "  CHECK PK CONS : existing_val = " + str( existing_val ) )
+                for i in existing_val :
+                  i = i.replace( "'", "" ).split( ":" )
+                  print "i0 = " + str( i[0] )
+                  print "i1 = " + str( i[1] )
+                  if k1 == i[0] and str( val[ k1 ] ) == i[1] :
+                    logging.debug( "  CHECK PK CONS : failed because '" + k1 + \
+                                   "' == '" + str( i[0] ) + \
+                                   "' and '" + str( val[ k1 ] ) + \
+                                   " == '" + str( i[1] ) + "'" )
+                    print "here2"
+                    return False
+
+    print "here1"
+    return True
+
+  ###########
+  #  IS PK  #
+  ###########
+  def is_pk( self, a_str ) :
+    '''check if the input string is a primary key'''
+
+    # TODO : generalize to primary keys with multiple atts
+    logging.debug( "  IS PK : a_str = " + str( a_str ) )
+
+    for i in self.pk_cons :
+      if a_str == i[1] :
+        return True
+    return False
+
+  #####################
+  #  CONVERT TO LIST  #
+  #####################
+  def convert_to_list( self, some_thing ) :
+    '''convert input string into a list'''
+
+    if some_thing.startswith( "{" ) :
+      some_thing = some_thing[ 1:-1 ]
+
+    some_thing = some_thing.translate( None, string.whitespace )
+    some_thing = some_thing.split( "," )
+    tmp = []
+
+    i = 0
+    while i < len( some_thing ) :
+      curr = some_thing[ i ]
+
+      if "[" in curr and not "]" in curr :
+        j = i + 1
+        while "[" in curr and not "]" in curr :
+          curr += "," + some_thing[ j ]
+          j += 1
+        i = j
+
+      tmp.append( curr )
+      i += 1
+
+    return tmp
+
+  ###################
+  #  CHECK FK CONS  #
+  ###################
+  def check_fk_cons( self, key, val ) :
+    '''check for foreign key constraint violations'''
+
+    logging.debug( "  CHECK FK CONS : key = " + str( key ) )
+    logging.debug( "  CHECK FK CONS : val = " + str( val ) + ", type = " + str( type( val ) ) )
+    logging.debug( "  CHECK FK CONS : fk_cons :")
+    for k in self.fk_cons :
+      logging.debug( k )
+
+    for k in val :
+      if self.is_fk( k ) :
+        fk_ref = self.is_fk( k )
+        if not val[k] in self.cursor.get( fk_ref[0] ) :
+          logging.debug( "  CHECK FK CONS : failed on foreign key '" + str( val[k] ) + "'" )
+          return False
+
+    return True
+
+  ###########
+  #  IS FK  #
+  ###########
+  def is_fk( self, a_str ) :
+    '''check if the input string is a foreign key'''
+
+    # TODO : generalize to primary keys with multiple atts
+    logging.debug( "  IS FK : a_str = " + str( a_str ) )
+
+    for i in self.fk_cons :
+      if a_str == i[0][1] :
+        return i[2]
+    return None
+
+  ###################
+  #  CHECK DM CONS  #
+  ###################
+  def check_dm_cons( self, key, val ) :
+    '''check for domain constraint violations'''
+
+    logging.debug( "  CHECK DM CONS : key = " + str( key ) )
+    logging.debug( "  CHECK DM CONS : val = " + str( val ) + ", type = " + str( type( val ) ) )
+    logging.debug( "  CHECK DM CONS : dm_cons :")
+    for k in self.dm_cons :
+      logging.debug( str( k ) + "," + str( self.dm_cons[k] ) )
+
+    # first check key
+    try :
+      if not "<type '" + self.dm_cons[ key ] + "'>" == str( type( val ) ) :
+        logging.debug( "  CHECK DM CONS : failed0" )
+        return False
+
+    # case key isn't set
+    except KeyError :
+      pass
+
+    # case key type violates a dm constraint
+    except ValueError :
+      logging.debug( "  CHECK DM CONS : failed1" )
+      return False
+
+    # then check val
+    if type( val ) == dict :
+      for k in val :
+        if not self.check_dm_cons( k, val[k] ) :
+          logging.debug( "  CHECK DM CONS : failed2" )
+          return False
+      return True
+
+    elif type( val ) == int :
+      if not self.dm_cons[ key ] == "int" :
+        logging.debug( "  CHECK DM CONS : failed3" )
+        return False
+
+    elif type( val ) == float :
+      if not self.dm_cons[ key ] == "float" :
+        logging.debug( "  CHECK DM CONS : failed4" )
+        return False
+
+    elif type( val ) == str :
+      if not self.dm_cons[ key ] == "str" :
+        logging.debug( "  CHECK DM CONS : failed5" )
+        return False
+
+    elif type( val ) == list :
+      if not self.dm_cons[ key ] == "list" :
+        logging.debug( "  CHECK DM CONS : failed6" )
+        return False
+
+    else :
+      raise ValueError( "unrecognized value type for '" + str( val ) + "'" )
+
+    return True
+
+  ###############
+  #  PARSE CON  #
+  ###############
+  def parse_con( self, con ) :
+    '''parse the constraint'''
+
+    con = con.translate( None, string.whitespace )
+
+    if "REF" in con :
+      con = con.split( "REF" )
+      return [ ast.literal_eval( con[0] ),
+               "REF",
+               ast.literal_eval( con[1] ) ]
+
+    else :
+      return ast.literal_eval( con )
+
 
 #########################
 #  THREAD OF EXECUTION  #
--- src/Test_conman_pickledb.py
+++ src/Test_conman_pickledb.py
@@ -8,8 +8,14 @@ Test_conman_pickldb.py
 #  IMPORTS  #
 #############
 # standard python packages
-import logging, os, pickledb, sys, time, unittest
+import logging
+import os
+import pickledb 
+import sys 
+import time 
+import unittest
 
+# custom libs
 import ConMan
 
 
@@ -21,15 +27,96 @@ class Test_conman_pickledb( unittest.TestCase ) :
   logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.DEBUG )
   #logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.INFO )
 
-  ##################
-  #  #
-  ##################
-  @unittest.skip( "stuff" )
-  def test_( self ) :
+  ###############
+  #  EXAMPLE 1  #
+  ###############
+  #@unittest.skip( "example 1." )
+  def test_example_1( self ) :
 
-    test_id    = "test_avg_pickledb"
+    test_id    = "test_example_1"
     NOSQL_TYPE = "pickledb"
+    DBNAME     = "../data/" + test_id + ".db"
 
+    # --------------------------------------------------------------- #
+    # create db instance
+
+    logging.info( "  " + test_id + ": initializing pickledb instance." )
+
+    if os.path.isfile( DBNAME ) :
+      os.remove( DBNAME )
+
+    dbInst = pickledb.load( DBNAME, False )
+
+    # --------------------------------------------------------------- #
+    # define cons
+
+    c = ConMan.ConMan( "pickledb", dbInst, "base_case" )
+    #c.set_cons( "['pk','author'] WHEN ['title','pubYear']" )
+    #c.set_cons( "['fk','author','authorid'] DELETE ON CASCADE" )
+
+    c.set_cons( "['pk','isbn']" )
+    c.set_cons( "['fk','aid'] REF ['authorid_list']" )
+
+    c.set_cons( "['dm','author','str']" )
+    c.set_cons( "['dm','title','str']" )
+    c.set_cons( "['dm','isbn','int']" )
+    c.set_cons( "['dm','pubYear','int']" )
+    c.set_cons( "['dm','numCopies','int']" )
+    c.set_cons( "['dm','categories','list']" )
+    c.set_cons( "['dm','cost(Dollars)','float']" )
+    c.set_cons( "['dm','authorid_list','list']" )
+    c.set_cons( "['dm','aid','int']" )
+
+    # --------------------------------------------------------------- #
+    # insert data
+
+    book1 = { "author"        : "Elsa Menzel",
+              "aid"           : 10, \
+              "title"         : "A Martial Arts Primer", \
+              "isbn"          : 0, \
+              "pubYear"       : 2018, \
+              "numCopies"     : 0, \
+              "categories"    : ["fantasy","action","lgbt"], \
+              "cost(Dollars)" : 10.00 }
+    book2 = { "author"        : "Anna Summers", \
+              "aid"           : 11, \
+              "title"         : "The Rising", \
+              "isbn"          : 1, \
+              "pubYear"       : 2017, \
+              "numCopies"     : 0, \
+              "categories"    : ["fantasy"], \
+              "cost(Dollars)" : 9.99 }
+    authorid_list = [ 10,11 ]
+#    book3 = { "author" : "Kat Green", \
+#              "title" : "Frozen Space", \
+#              "pubYear" : 2016, \
+#              "numCopies" : 0, \
+#              "categories" : ["fantasy", "scifi"], \
+#              "cost(Dollars)" : 0 }
+
+    c.insert( "authorid_list", authorid_list )
+    c.insert( "bid1", book1 )
+    c.insert( "bid2", book2 )
+#    c.insert( "bid3", book3 )
+
+    logging.debug( "  TEST EXAMPLE 1 : data dump :" )
+    for k in dbInst.getall() :
+      logging.debug( "key = " + k + ", val = " + str( dbInst.get( k ) ) )
+
+    self.assertEqual( 0, 0 )
+
+    # ---------------------------------------------------- #
+    # close pickle db instance
+
+    dbInst.deldb()
+
+
+#########################
+#  THREAD OF EXECUTION  #
+#########################
+if __name__ == "__main__" :
+
+  unittest.main()
 
 #########
 #  EOF  #
